628ebcb57f725b5fa4845394b42f09a8bf525520
Sandip Gyawali <sandipgyawali10@gmail.com>
2024-06-03 22:22:44 +0545

    basic theme, config module refactor

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    APPWRITE_URL: "https://cloud.appwrite.io/v1",
+    APPWRITE_PROJECT_ID: "665d49ce002ff4b3a1c2",
+  },
+};
 
 export default nextConfig;
